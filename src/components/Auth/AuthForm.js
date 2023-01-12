5e59770009165a14413bac9875520f4f97545cf4
Michael Rideout <rideout.m@hotmail.com>
2023-01-11 21:35:16 -0500

    finish auth

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,34 +1,88 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const history = useHistory();
+
+  // const navigate = useNavigate();
+  const authCtx = useContext(AuthContext);
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAu0v8DQadUhvJLWm4XBPtpBSTcE1zdSeU";
+    } else {
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAu0v8DQadUhvJLWm4XBPtpBSTcE1zdSeU";
+    }
+
+    fetch(url, {
+      method: "post",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => {
+        setIsLoading(false);
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((data) => {
+            let errorMsg = "Authenticated failed.";
+            // if (data && data.error && data.error.message) {
+            //   errorMsg = data.error.message;
+            // }
+
+            throw new Error(errorMsg);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(new Date().getTime() + +data.expiresIn * 1000);
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        history.replace("/");
+      })
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+          {!isLoading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {isLoading && <p>Sending Request</p>}
+          <button type="button" className={classes.toggle} onClick={switchAuthModeHandler}>
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,43 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useContext, useRef } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAu0v8DQadUhvJLWm4XBPtpBSTcE1zdSeU", {
+      method: "post",
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+      history.replace("/");
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
